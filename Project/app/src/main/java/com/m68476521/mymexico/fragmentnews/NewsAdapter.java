42b60dd7413006066312605e095a7f9076a1105c
Mike <m68476521.1@gmail.com>
2018-02-09 20:39:11 -0800

    feat: cardview updated

--- Project/app/src/main/java/com/m68476521/mymexico/fragmentnews/NewsAdapter.java
+++ Project/app/src/main/java/com/m68476521/mymexico/fragmentnews/NewsAdapter.java
@@ -3,7 +3,6 @@ package com.m68476521.mymexico.fragmentnews;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
-import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
@@ -15,7 +14,6 @@ import android.widget.TextView;
 
 import com.m68476521.mymexico.R;
 import com.m68476521.mymexico.data.TaskContract;
-import com.m68476521.mymexico.databinding.CardNewsBinding;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -42,19 +40,17 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         cursor.moveToPosition(position);
 
         String title = cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_NAME));
+        Log.d("MIKE title", title);
         holder.textViewTitle.setText(title);
 
         String desc = cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_DESCRIPTION));
         holder.textViewShortDesc.setText(desc);
-
-        int indexId = cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_ID);
-        final String id = cursor.getString(indexId);
+        Log.d("MIKE desc", desc);
 
         Picasso.with(context)
                 .load(R.drawable.ic_launcher_background)
                 .placeholder(R.mipmap.ic_launcher)
                 .into(holder.imageViewBackGround);
-
 
         String image = cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_IMAGE));
         if (TextUtils.isEmpty(image)) {
@@ -80,18 +76,12 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         public final TextView textViewTitle;
         public final TextView textViewShortDesc;
         public final ImageView imageViewBackGround;
-        private CardNewsBinding cardNewsBinding;
 
         public ViewHolder(View view) {
             super(view);
-            cardNewsBinding = DataBindingUtil.bind(view);
             textViewTitle = view.findViewById(R.id.text_view_title);
             textViewShortDesc = view.findViewById(R.id.text_view_short_desc);
             imageViewBackGround = view.findViewById(R.id.image_view_poster);
-        }
-
-        public CardNewsBinding getBinding() {
-            return cardNewsBinding;
         }
     }
 
