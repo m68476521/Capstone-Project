ed43172c0107953c70f46242c78c36ccba25df95
Mike <m68476521.1@gmail.com>
2018-02-13 22:29:12 -0800

    feat: fragment trick added
    the functionality was added for the tricks

--- Project/app/src/main/java/com/m68476521/mymexico/fragmenttrick/FragmentTrick.java
+++ Project/app/src/main/java/com/m68476521/mymexico/fragmenttrick/FragmentTrick.java
@@ -1,18 +1,28 @@
 package com.m68476521.mymexico.fragmenttrick;
 
+import android.database.ContentObserver;
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.m68476521.mymexico.R;
+import com.m68476521.mymexico.data.TaskContract;
 import com.m68476521.mymexico.databinding.FragmentTrickBinding;
+import com.m68476521.mymexico.fragmentfav.FragmentFavorite;
 
 public class FragmentTrick extends Fragment {
 
     private FragmentTrickBinding fragmentTrickBinding;
+    private Cursor cursor;
+    private TrickAdapter trickAdapter;
 
     public FragmentTrick() {
         // Required empty public constructor
@@ -21,6 +31,26 @@ public class FragmentTrick extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MIKE on create", "TRicks");
+        cursor = getContext().getContentResolver().query(TaskContract.TaskEntry.CONTENT_URI_TRICKS,
+                null,
+                null,
+                null,
+                TaskContract.TaskEntry.COLUMN_ID);
+
+        if (cursor != null || cursor.getCount() != 0 ) {
+            Log.d("Mike", "cursor in ont null at Tricks");
+            for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                int descriptionIndex = cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_FCM_QUESTION);
+                String name = cursor.getString(descriptionIndex);
+                Log.d("MIKE", "data got it from tricks " + name);
+            }
+        }
+        MyObserver myObserver = new MyObserver(new Handler());
+        getContext().getContentResolver().registerContentObserver(
+                TaskContract.TaskEntry.CONTENT_URI_TRICKS,
+                true, myObserver
+        );
     }
 
     @Override
@@ -28,6 +58,56 @@ public class FragmentTrick extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         fragmentTrickBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_trick, container, false);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+
+        fragmentTrickBinding.recyclerViewTrick.setHasFixedSize(true);
+
+        trickAdapter = new TrickAdapter(getActivity(), new TrickAdapter.OnItemClicked() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+            }
+        });
+
+        fragmentTrickBinding.recyclerViewTrick.setAdapter(trickAdapter);
+
+        fragmentTrickBinding.recyclerViewTrick.setLayoutManager(linearLayoutManager);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            setupAdapterByCursor(cursor);
+        }
+
         return fragmentTrickBinding.getRoot();
+    }
+
+    private void setupAdapterByCursor(Cursor cursor) {
+        trickAdapter.swapCursor(cursor);
+    }
+
+    class MyObserver extends ContentObserver {
+        public MyObserver(Handler handler) {
+            super(handler);
+            Log.d("MIKE", "on CreateObserveTrickr");
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            Log.d("MIKE", "onChangeAtrick");
+        }
+
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            Log.d("MIKE", "onChangeTrick");
+            cursor = getContext().getContentResolver().query(TaskContract.TaskEntry.CONTENT_URI_TRICKS,
+                    null,
+                    null,
+                    null,
+                    TaskContract.TaskEntry.COLUMN_ID);
+            setupAdapterByCursor(cursor);
+        }
     }
 }
---
+++ Project/app/src/main/java/com/m68476521/mymexico/fragmenttrick/TrickAdapter.java
@@ -0,0 +1,108 @@
+package com.m68476521.mymexico.fragmenttrick;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.m68476521.mymexico.R;
+import com.m68476521.mymexico.data.TaskContract;
+import com.squareup.picasso.Picasso;
+
+/**
+ * TODO: add description
+ */
+
+public class TrickAdapter extends RecyclerView.Adapter<TrickAdapter.ViewHolder> {
+    private Context context;
+    private Cursor cursor;
+
+    private final OnItemClicked listener;
+
+    public TrickAdapter(Activity context, OnItemClicked listener) {
+        this.listener = listener;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_trick, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        cursor.moveToPosition(position);
+
+        String title = cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_FCM_QUESTION));
+        Log.d("MIKE title", title);
+        holder.textViewTitle.setText(title);
+
+        Picasso.with(context)
+                .load(R.drawable.ic_launcher_background)
+                .placeholder(R.mipmap.ic_launcher)
+                .into(holder.imageViewBackGround);
+
+        String image = cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_FCM_FAKE_ANS_A));
+        if (TextUtils.isEmpty(image)) {
+            holder.imageViewBackGround.setVisibility(View.GONE);
+        } else {
+            Picasso.with(context)
+                    .load(image)
+                    .placeholder(R.mipmap.ic_launcher)
+                    .into(holder.imageViewBackGround);
+        }
+
+        holder.imageViewFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MIKE", "clicked fav");
+                listener.onItemClick(v, position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        if (cursor != null) {
+            return cursor.getCount();
+        } else {
+            return 0;
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView textViewTitle;
+        public final ImageView imageViewBackGround;
+        public final ImageView imageViewFavorite;
+
+        public ViewHolder(View view) {
+            super(view);
+            textViewTitle = view.findViewById(R.id.text_view_title);
+            imageViewBackGround = view.findViewById(R.id.image_view_poster);
+            imageViewFavorite = view.findViewById(R.id.image_view_fav);
+        }
+    }
+
+    public void swapCursor(Cursor cursor) {
+        if (this.cursor == cursor) {
+            return;
+        }
+        this.cursor = cursor;
+        if (cursor != null) {
+            this.notifyDataSetChanged();
+        }
+    }
+
+    public interface OnItemClicked {
+        void onItemClick(View view, int position);
+    }
+}
