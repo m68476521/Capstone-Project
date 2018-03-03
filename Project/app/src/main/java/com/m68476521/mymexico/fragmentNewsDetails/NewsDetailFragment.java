2476d1bcbe71742ef7751cef81bc21236bdb7f9d
Mike <m68476521.1@gmail.com>
2018-03-02 20:56:40 -0800

    feat: adding CollapsingToolbarLayout into Fragment Details

--- Project/app/src/main/java/com/m68476521/mymexico/fragmentNewsDetails/NewsDetailFragment.java
+++ Project/app/src/main/java/com/m68476521/mymexico/fragmentNewsDetails/NewsDetailFragment.java
@@ -1,10 +1,14 @@
 package com.m68476521.mymexico.fragmentNewsDetails;
 
 import android.database.Cursor;
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.transition.TransitionInflater;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,6 +19,7 @@ import android.widget.TextView;
 
 import com.m68476521.mymexico.R;
 import com.m68476521.mymexico.data.TaskContract;
+import com.m68476521.mymexico.databinding.FragmentNewsDetailBinding;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
@@ -30,6 +35,8 @@ public class NewsDetailFragment extends Fragment {
     private static final String EXTRA_SECTION_FAVORITES = "FAVORITES";
     private Cursor cursor;
     private String section;
+
+    private FragmentNewsDetailBinding binding;
 
     public NewsDetailFragment() {
         // Required empty public constructor
@@ -62,12 +69,8 @@ public class NewsDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_news_detail, container, false);
-    }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_news_detail, container, false);
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
         if (section.equals(EXTRA_SECTION_NEWS)) {
             cursor = getContext().getContentResolver().query(TaskContract.TaskEntry.CONTENT_URI,
                     null,
@@ -82,34 +85,54 @@ public class NewsDetailFragment extends Fragment {
                     TaskContract.TaskEntry.COLUMN_ID);
         }
 
-
         Integer position = getArguments().getInt(EXTRA_NEWS_POSITION);
         String transitionName = getArguments().getString(EXTRA_TRANSITION_NAME);
 
         cursor.moveToPosition(position);
 
-        TextView nameTextView = view.findViewById(R.id.news_name_text);
+        final String title = cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_NAME));
 
-        String title = cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_NAME));
+        assert binding.toolbar != null;
+        binding.toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
 
-        nameTextView.setText(title);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(binding.toolbar);
+        assert binding.collapsingToolbar != null;
+        binding.collapsingToolbar.setTitle(title);
+        binding.collapsingToolbar.setCollapsedTitleTextColor(Color.WHITE);
+        binding.collapsingToolbar.setExpandedTitleColor(Color.WHITE);
+        assert binding.appbar != null;
+        binding.appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
 
-        TextView detailTextView = view.findViewById(R.id.news_detail_text);
-        detailTextView.setText(cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_DESCRIPTION)));
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    binding.collapsingToolbar.setTitle(title);
+                    isShow = true;
+                } else if (isShow) {
+                    binding.collapsingToolbar.setTitle(title);
+                    isShow = false;
+                }
+            }
+        });
 
-        ImageView imageView = view.findViewById(R.id.news_detail_image_view);
+        binding.newsDetailText.setText(cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_DESCRIPTION)));
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Log.d("MIKE tag", "B");
-            imageView.setTransitionName(transitionName);
+            binding.newsDetailImageView.setTransitionName(transitionName);
         }
 
-        TextView category = view.findViewById(R.id.category_text_view);
-        category.setText("CategoryExample");
+        binding.categoryTextView.setText("CategoryExample");
 
         Picasso.with(getContext())
                 .load(cursor.getString(cursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_IMAGE)))
                 .noFade()
-                .into(imageView, new Callback() {
+                .into(binding.newsDetailImageView, new Callback() {
                     @Override
                     public void onSuccess() {
                         startPostponedEnterTransition();
@@ -120,5 +143,6 @@ public class NewsDetailFragment extends Fragment {
                         startPostponedEnterTransition();
                     }
                 });
+        return binding.getRoot();
     }
 }
